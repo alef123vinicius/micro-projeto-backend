ece0ea6f4b976982ff11985dafab4a7d8c8d873a
alef123vinicius <alef123.vinicius@gmail.com>
2023-05-10 21:56:45 -0300

    dokerfile

--- src/test/java/br/com/microprojeto/microprojetobackend/MicroProjetoBackendApplicationTests.java
+++ src/test/java/br/com/microprojeto/microprojetobackend/MicroProjetoBackendApplicationTests.java
@@ -3,11 +3,14 @@ package br.com.microprojeto.microprojetobackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.MOCK, classes = MicroProjetoBackendApplication.class)
 class MicroProjetoBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		String teste = "";
+
 	}
 
 }
